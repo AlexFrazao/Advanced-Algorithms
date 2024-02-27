50689b8d30319e5b5847d7c44649b739c3b053b7
Alexandre Filipe Pascoal Frazão <ist1111098@sangria.scdeec.tecnico.ulisboa.pt>
2024-02-27 11:48:35 +0000

    Added pdf functions and started adding A node array

--- P1.c
+++ P1.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node* node;
+typedef struct node *node;
 
-struct node {
+struct node
+{
     int v;
     node child;
     node brother;
 };
 
-node new_node(){
+node new_node()
+{
     node new_node = (node)malloc(sizeof(struct node));
     new_node->v = 1;
     new_node->child = NULL;
@@ -17,19 +19,91 @@ node new_node(){
     return new_node;
 }
 
-void V(node node_to_be_changed, int new_v){
-    if(new_v >= 0){
-        node_to_be_changed->v = new_v;
-    }else{
-        node_to_be_changed->v = abs(new_v);
+int ptr2loc(node v, node A)
+{
+    int r;
+    r = -1;
+    if (NULL != v)
+        r = ((size_t)v - (size_t)A) / sizeof(struct node); //size_t retrieves the size of an array // this computation retrieves the byte difference between the node being tested and A
+    return (int)r;
+}
 
+void S(node v, node A)
+{
+    int f;
+    if (NULL == v)
+        printf("NULL\n");
+    else
+    {
+        printf("node: %d ", ptr2loc(v, A));
+        printf("v: %d ", v->v);
+        printf("child: %d ", ptr2loc(v->child, A));
+        printf("brother: %d ", ptr2loc(v->brother, A));
+        printf("\n");
     }
 }
 
-int main() {
+void P(node)
+{
+
+    /*The showList function that gives a description of the brother
+list at a node, i.e., it calls showNode for the current node and then
+recursively calls showList on the brother node if the current v value is
+not negative.*/
+}
+
+void V(node node_to_be_changed, int new_v)
+{
+    /**/
+
+    if (new_v >= 0)
+    {
+        node_to_be_changed->v = new_v;
+    }
+    else
+    {
+        node_to_be_changed->v = abs(new_v);
+    }
+}
+
+void U(heap, heap)
+{ /*The Meld function is used to join two heaps, i.e., merge
+the two roots into a single tree. This function returns the root of the
+resulting tree.*/
+}
+
+void R(root, node, v)
+{ /*The DecreaseKey function is used to decrease the v
+field of the current node. This node may be part of a meldable heap
+tree and therefore this operation might need modify this tree. The
+root of the corresponding tree must be given as the first argument to
+this function.*/
+}
+
+void M(node)
+{ /*The Min function returns the absolute value of v for the cur-
+rent node. When the argument node is a root the result is the heap
+minimum value.*/
+}
+
+void E(root)
+{ /*The ExtractMin function removes the root node from the current
+heap. The function returns the, possibly new, identification of the
+resulting tree.*/
+}
+
+int main()
+{
     node root = new_node();
     root->child = new_node();
     root->child->brother = new_node();
+
+    int n;           // Declare the number of nodes
+    node A;          // Declare A as a pointer to an array of struct node
+    
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    A = (node)calloc(n, sizeof(struct node));
 
     V(root, 1);
     V(root->child, 2);
@@ -41,3 +115,5 @@ int main() {
 
     return 0;
 }
+
+
